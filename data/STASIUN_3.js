92128f2ff420d5efc2a4fcdb5297910c63e9edfa
ardiansyahgeo <67992886+ardiansyahgeo@users.noreply.github.com>
2021-03-13 21:13:11 +0700

    Add files via upload

---
+++ data/STASIUN_3.js
@@ -0,0 +1,13 @@
+var json_STASIUN_3 = {
+"type": "FeatureCollection",
+"name": "STASIUN_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMOBJ": "Stasiun Negararatu", "FGSSTA": "0", "KLSSTA": "0", "FCODE": null, "KODKOD": null, "WILSTA": null, "DOPSTA": null, "KMTSTA": null, "LINSTA": null, "KONKON": "0", "REMARK": "Stasiun Kereta Api", "SRS_ID": null, "LCODE": "CA0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 104.737725434000026, -4.65664539 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Stasiun Kalibalangan", "FGSSTA": "0", "KLSSTA": "0", "FCODE": null, "KODKOD": null, "WILSTA": null, "DOPSTA": null, "KMTSTA": null, "LINSTA": null, "KONKON": "0", "REMARK": "Stasiun Kereta Api", "SRS_ID": null, "LCODE": "CA0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 104.972825613999987, -4.866676406000011 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Statsiun Kereta Api Blambangan Pagar", "FGSSTA": "0", "KLSSTA": "0", "FCODE": null, "KODKOD": null, "WILSTA": null, "DOPSTA": null, "KMTSTA": null, "LINSTA": null, "KONKON": "0", "REMARK": "Stasiun Kereta Api", "SRS_ID": null, "LCODE": "CA0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 105.0389173, -4.8953206 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Statsiun Kereta Api Negeriagung", "FGSSTA": "0", "KLSSTA": "0", "FCODE": null, "KODKOD": null, "WILSTA": null, "DOPSTA": null, "KMTSTA": null, "LINSTA": null, "KONKON": "0", "REMARK": "Stasiun Kereta Api", "SRS_ID": null, "LCODE": "CA0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 104.605811303999985, -4.572496495 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Stasiun Kotabumi", "FGSSTA": "0", "KLSSTA": "0", "FCODE": null, "KODKOD": null, "WILSTA": null, "DOPSTA": null, "KMTSTA": null, "LINSTA": null, "KONKON": "0", "REMARK": "Stasiun Kereta Api", "SRS_ID": null, "LCODE": "CA0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 104.88113686299999, -4.821957935 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Stasiun Tulungbuyut", "FGSSTA": "0", "KLSSTA": "0", "FCODE": null, "KODKOD": null, "WILSTA": null, "DOPSTA": null, "KMTSTA": null, "LINSTA": null, "KONKON": "0", "REMARK": "Stasiun Kereta Api", "SRS_ID": null, "LCODE": "CA0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 104.664537543999984, -4.625837879000017 ] } }
+]
+}
